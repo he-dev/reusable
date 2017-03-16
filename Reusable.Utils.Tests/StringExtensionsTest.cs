abc6ac14c47a65c098a1f4a1ebf07a76370ac9f7
MTk4MQ <bennyboss+github@gmail.com>
2017-03-16 18:18:34 +0100

    improvements

---
+++ Reusable.Utils.Tests/StringExtensionsTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Reusable.Tests
+{
+    [TestClass]
+    public class StringExtensionsTest
+    {
+        [TestMethod]
+        public void NonEmptyOrNull_Empty_Null()
+        {
+            Assert.IsNull(string.Empty.NonEmptyOrNull());
+        }
+
+        [TestMethod]
+        public void NonEmptyOrNull_Null_Null()
+        {
+            Assert.IsNull(((string)null).NonEmptyOrNull());
+        }
+
+        [TestMethod]
+        public void NonEmptyOrNull_NonEmpty_NonEmpty()
+        {
+            Assert.AreEqual("foo", "foo".NonEmptyOrNull());
+        }
+
+
+        [TestMethod]
+        public void NonWhitespaceOrNull_Empty_Null()
+        {
+            Assert.IsNull(string.Empty.NonWhitespaceOrNull());
+        }
+
+        [TestMethod]
+        public void NonWhitespaceOrNull_Null_Null()
+        {
+            Assert.IsNull(((string)null).NonWhitespaceOrNull());
+        }
+
+        [TestMethod]
+        public void NonWhitespaceOrNull_Whitespace_Null()
+        {
+            Assert.IsNull(((string)"    ").NonWhitespaceOrNull());
+        }
+
+        [TestMethod]
+        public void NonWhitespaceOrNull_NonEmpty_NonEmpty()
+        {
+            Assert.AreEqual("foo", "foo".NonWhitespaceOrNull());
+        }
+    }
+}
---
+++ Reusable.Utils/_codebase/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reusable
+{
+    public static class StringExtensions
+    {
+        public static string NonEmptyOrNull(this string value) => string.IsNullOrEmpty(value) ? null : value;
+
+        public static string NonWhitespaceOrNull(this string value) => string.IsNullOrWhiteSpace(value) ? null : value;
+    }
+}
