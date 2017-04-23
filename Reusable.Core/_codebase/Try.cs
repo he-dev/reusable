92390b4b8e8b44d52919eec8fef6d79b090c0b42
MTk4MQ <bennyboss+github@gmail.com>
2017-04-23 17:22:25 +0200

    - added success

--- Reusable.Core/_codebase/Try.cs
+++ Reusable.Core/_codebase/Try.cs
@@ -38,33 +38,33 @@ namespace Reusable
             }
         }
 
-        public static void Execute<TLocal>(Action action, Func<TLocal> preAction, Action<TLocal> postAction)
+        public static void Execute<TLocal>(Action action, Func<TLocal> preAction, Action<TLocal, bool> postAction)
         {
             var local = preAction();
             try
             {
                 action();
-                postAction(local);
+                postAction(local, true);
             }
             catch (Exception)
             {
-                postAction(local);
+                postAction(local, false);
                 throw;
             }
         }
 
-        public static TResult Execute<TLocal, TResult>(Func<TResult> action, Func<TLocal> preAction, Action<TLocal> postAction)
+        public static TResult Execute<TLocal, TResult>(Func<TResult> action, Func<TLocal> preAction, Action<TLocal, bool> postAction)
         {
             var local = preAction();
             try
             {
                 var result = action();
-                postAction(local);
+                postAction(local, true);
                 return result;
             }
             catch (Exception)
             {
-                postAction(local);
+                postAction(local, false);
                 throw;
             }
         }
