e43011fc3a08a9dfc7d8e99674c178d6c136c562
x-t3ch <bennyboss+github@gmail.com>
2017-11-24 12:09:24 +0100

    new logger-factory extensions

---
+++ Reusable.OmniLog/TehCodez/LoggerFactoryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using JetBrains.Annotations;
+using Reusable.Extensions;
+
+namespace Reusable.OmniLog
+{
+    public static class LoggerFactoryExtensions
+    {
+        [NotNull]
+        public static ILogger CreateLogger<T>([NotNull] this ILoggerFactory loggerFactory)
+        {
+            if (loggerFactory == null) throw new ArgumentNullException(nameof(loggerFactory));
+
+            return loggerFactory.CreateLogger(typeof(T).ToPrettyString());
+        }
+    }
+}
