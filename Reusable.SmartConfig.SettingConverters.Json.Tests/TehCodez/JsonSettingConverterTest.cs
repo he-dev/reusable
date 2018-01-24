ae5821c0bdef939d107c826938c4bd782ed4bbfe
he-dev <bennyboss+github@gmail.com>
2018-01-24 14:38:54 +0100

    added tests

---
+++ Reusable.SmartConfig.SettingConverters.Json.Tests/TehCodez/JsonSettingConverterTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Reusable.SmartConfig.SettingConverters.Json.Tests
+{
+    [TestClass]
+    public class JsonSettingConverterTest
+    {
+        private static readonly ISettingConverter Converter = new JsonSettingConverter();
+
+        [TestMethod]
+        public void Serialize_BasicTypes_Strings()
+        {
+            var testValues = new (object Value, string Expected)[]
+            {
+
+            };
+        }
+    }
+}
