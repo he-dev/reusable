5d1228e32974a40564d1529a8da0e5676b2d76b3
he-dev <bennyboss+github@gmail.com>
2018-06-07 14:33:49 +0200

    fixed json serializer extensions

---
+++ Reusable.ThirdParty.JsonNetUtilities/src/Extensions/SerializerExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+
+namespace Reusable.Utilities.JsonNet.Extensions
+{
+    public static class SerializerExtensions
+    {
+        [NotNull]
+        public static byte[] SerializeToBytes<T>([NotNull] this JsonSerializer jsonSerializer, [NotNull] T obj)
+        {
+            if (jsonSerializer == null) throw new ArgumentNullException(nameof(jsonSerializer));
+            if (obj == null) throw new ArgumentNullException(nameof(obj));
+
+            using (var memoryStream = new MemoryStream())
+            using (var textWriter = new StreamWriter(memoryStream))
+            using (var jsonWriter = new JsonTextWriter(textWriter))
+            {
+                jsonSerializer.Serialize(jsonWriter, new object());
+                return memoryStream.GetBuffer();
+            }
+        }
+
+        [NotNull]
+        public static T Deserialize<T>([NotNull] this JsonSerializer jsonSerializer, [NotNull] byte[] body)
+        {
+            if (jsonSerializer == null) throw new ArgumentNullException(nameof(jsonSerializer));
+            if (body == null) throw new ArgumentNullException(nameof(body));
+
+            using (var memoryStream = new MemoryStream(body))
+            using (var streamReader = new StreamReader(memoryStream))
+            using (var jsonTextReader = new JsonTextReader(streamReader))
+            {
+                return jsonSerializer.Deserialize<T>(jsonTextReader);
+            }
+        }
+    }
+}
