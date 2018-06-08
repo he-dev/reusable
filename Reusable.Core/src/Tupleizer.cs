0651e96fc2bd3a596236face2b7a7d20a9b09aa0
ᎬᏁᎶᎥᏁᎬᎬᏒ <bennyboss+github@gmail.com>
2018-06-08 14:56:18 +0200

    added tupleizer

---
+++ Reusable.Core/src/Tupleizer.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Reusable
+{
+    public static class StringExtensions
+    {
+        public static Tuple<bool, Tuple<T1, T2>> Parse<T1, T2>(this string input, string pattern, RegexOptions options = RegexOptions.None)
+        {
+            return input.Parse(pattern, options).Tupleize<T1, T2>();
+        }
+
+        public static Tuple<bool, Tuple<T1, T2, T3>> Parse<T1, T2, T3>(this string input, string pattern, RegexOptions options = RegexOptions.None)
+        {
+            return input.Parse(pattern, options).Tupleize<T1, T2, T3>();
+        }
+
+        public static Tuple<bool, Tuple<T1, T2, T3, T4>> Parse<T1, T2, T3, T4>(this string input, string pattern, RegexOptions options = RegexOptions.None)
+        {
+            return input.Parse(pattern, options).Tupleize<T1, T2, T3, T4>();
+        }
+
+        public static Tuple<bool, Tuple<T1, T2, T3, T4, T5>> Parse<T1, T2, T3, T4, T5>(this string input, string pattern, RegexOptions options = RegexOptions.None)
+        {
+            return input.Parse(pattern, options).Tupleize<T1, T2, T3, T4, T5>();
+        }
+
+        public static Tuple<bool, Tuple<T1, T2, T3, T4, T5, T6>> Parse<T1, T2, T3, T4, T5, T6>(this string input, string pattern, RegexOptions options = RegexOptions.None)
+        {
+            return input.Parse(pattern, options).Tupleize<T1, T2, T3, T4, T5, T6>();
+        }
+
+        public static Tuple<bool, Tuple<T1, T2, T3, T4, T5, T6, T7>> Parse<T1, T2, T3, T4, T5, T6, T7>(this string input, string pattern, RegexOptions options = RegexOptions.None)
+        {
+            return input.Parse(pattern, options).Tupleize<T1, T2, T3, T4, T5, T6, T7>();
+        }
+
+        private static IDictionary<int, object> Parse(this string input, string pattern, RegexOptions options)
+        {
+            if (string.IsNullOrEmpty(input)) throw new ArgumentException($"{nameof(input)} must not be null or empty.");
+            if (string.IsNullOrEmpty(pattern)) throw new ArgumentException($"{nameof(pattern)} must not be null or empty.");
+
+            var inputMatch = Regex.Match(input, pattern, RegexOptions.ExplicitCapture | options);
+
+            var result =
+                inputMatch.Success
+                    ? inputMatch
+                        .Groups
+                        .Cast<Group>()
+                        // First group is the entire match. We don't need it.
+                        .Skip(1)
+                        .Where(g => g.Success)
+                        .Select(g =>
+                        {
+                            var ordinal = Regex.Match(g.Name, @"^(?:T(?<ordinal>\d+))").Groups["ordinal"];
+                            return
+                            (
+                                Ordinal:
+                                    ordinal.Success
+                                        ? int.TryParse(ordinal.Value, out var x) && x > 0 ? x : throw new ArgumentException($"Invalid 'Tx'. 'x' must be greater than 0.")
+                                        : throw new ArgumentException("Invalid group name. It must start with 'Tx' where 'x' is the ordinal of the T parameter and must be greater than 0."),
+                                Value:
+                                    string.IsNullOrEmpty(g.Value)
+                                        ? null
+                                        : g.Value
+                            );
+                        })
+                        .ToDictionary(
+                            g => g.Ordinal,
+                            g => (object)g.Value
+                        )
+                    : new Dictionary<int, object>();
+
+            result[0] = inputMatch.Success;
+
+            return result;
+        }
+    }
+
+    internal static class Tupleizer
+    {
+        public static Tuple<bool, Tuple<T1, T2>> Tupleize<T1, T2>(this IDictionary<int, object> data)
+        {
+            return
+                Tuple.Create(
+                    data.GetValue<bool>(0),
+                    Tuple.Create(
+                        data.GetValue<T1>(1),
+                        data.GetValue<T2>(2)
+                    )
+                );
+        }
+
+        public static Tuple<bool, Tuple<T1, T2, T3>> Tupleize<T1, T2, T3>(this IDictionary<int, object> data)
+        {
+            return
+                Tuple.Create(
+                    data.GetValue<bool>(0),
+                    Tuple.Create(
+                        data.GetValue<T1>(1),
+                        data.GetValue<T2>(2),
+                        data.GetValue<T3>(3)
+                    )
+                );
+        }
+
+        public static Tuple<bool, Tuple<T1, T2, T3, T4>> Tupleize<T1, T2, T3, T4>(this IDictionary<int, object> data)
+        {
+            return
+                Tuple.Create(
+                    data.GetValue<bool>(0),
+                    Tuple.Create(
+                        data.GetValue<T1>(1),
+                        data.GetValue<T2>(2),
+                        data.GetValue<T3>(3),
+                        data.GetValue<T4>(4)
+                    )
+                );
+        }
+
+        public static Tuple<bool, Tuple<T1, T2, T3, T4, T5>> Tupleize<T1, T2, T3, T4, T5>(this IDictionary<int, object> data)
+        {
+            return
+                Tuple.Create(
+                    data.GetValue<bool>(0),
+                    Tuple.Create(
+                        data.GetValue<T1>(1),
+                        data.GetValue<T2>(2),
+                        data.GetValue<T3>(3),
+                        data.GetValue<T4>(4),
+                        data.GetValue<T5>(5)
+                    )
+                );
+        }
+
+        public static Tuple<bool, Tuple<T1, T2, T3, T4, T5, T6>> Tupleize<T1, T2, T3, T4, T5, T6>(this IDictionary<int, object> data)
+        {
+            return
+                Tuple.Create(
+                    data.GetValue<bool>(0),
+                    Tuple.Create(
+                        data.GetValue<T1>(1),
+                        data.GetValue<T2>(2),
+                        data.GetValue<T3>(3),
+                        data.GetValue<T4>(4),
+                        data.GetValue<T5>(5),
+                        data.GetValue<T6>(6)
+                    )
+                );
+        }
+
+        public static Tuple<bool, Tuple<T1, T2, T3, T4, T5, T6, T7>> Tupleize<T1, T2, T3, T4, T5, T6, T7>(this IDictionary<int, object> data)
+        {
+            return
+                Tuple.Create(
+                    data.GetValue<bool>(0),
+                    Tuple.Create(
+                        data.GetValue<T1>(1),
+                        data.GetValue<T2>(2),
+                        data.GetValue<T3>(3),
+                        data.GetValue<T4>(4),
+                        data.GetValue<T5>(5),
+                        data.GetValue<T6>(6),
+                        data.GetValue<T7>(7)
+                    )
+                );
+        }
+
+        private static T GetValue<T>(this IDictionary<int, object> data, int itemIndex)
+        {
+            if (!data.TryGetValue(itemIndex, out var value) || value is null)
+            {
+                return default;
+            }
+            else
+            {
+                var isNullable =
+                    typeof(T).IsGenericType &&
+                    typeof(T).GetGenericTypeDefinition() == typeof(Nullable<>);
+
+                var targetType =
+                    isNullable
+                        ? typeof(T).GetGenericArguments().Single()
+                        : typeof(T);
+
+                return (T)Convert.ChangeType(value, targetType);
+            }
+        }
+    }
+}
---
+++ Reusable.Tests/src/Core/TupleizerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Reusable.Tests
+{
+    [TestClass]
+    public class TupleizerTest
+    {
+        [TestMethod]
+        public void Parse_TwoValues_TwoResults()
+        {
+            var (success, (name, count)) = "foo3".Parse<string, int?>(@"(?<T1>(?i:[a-z]+))(?<T2>\d+)?");
+
+            Assert.IsTrue(success);
+            Assert.AreEqual("foo", name);
+            Assert.AreEqual(3, count);
+        }
+
+        [TestMethod]
+        public void Parse_OneValue_OneResult()
+        {
+            var (success, (name, count)) = "foo".Parse<string, int?>(@"(?<T1>(?i:[a-z]+))(?<T2>\d+)?");
+
+            Assert.IsTrue(success);
+            Assert.AreEqual("foo", name);
+            Assert.IsNull(count);
+        }
+
+        [TestMethod]
+        public void Parse_NoValues_NoResults()
+        {
+            var (success, (name, count)) = "-".Parse<string, int?>(@"(?<T1>(?i:[a-z]+))(?<T2>\d+)?");
+
+            Assert.IsFalse(success);
+            Assert.IsNull(name);
+            Assert.IsNull(count);
+        }
+    }
+}
