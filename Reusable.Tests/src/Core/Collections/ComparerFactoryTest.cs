a1462b02b020fee8d01f2c579e5b9c459359a588
ᎬᏁᎶᎥᏁᎬᎬᏒ <bennyboss+github@gmail.com>
2018-11-25 19:26:17 +0100

    Clean-up comparer-factory

---
+++ Reusable.Tests/src/Core/Collections/ComparerFactoryTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reusable.Collections;
+using Reusable.Utilities.MSTest;
+
+namespace Reusable.Tests.Collections
+{
+    [TestClass]
+    public class ComparerFactoryTest
+    {
+        [TestMethod]
+        public void CanCompareSelectedValue()
+        {
+            var comparer = ComparerFactory<User>.Create(x => x.Age);
+
+            Assert.That.Comparer().IsCanonical
+            (
+                value: new User(20),
+                less: new User(15),
+                equal: new User(20),
+                greater: new User(30),
+                comparer
+            );
+
+            Assert.IsTrue(comparer.Compare(default, new User(0)) < 0);
+            Assert.IsTrue(comparer.Compare(new User(0), default) > 0);
+            Assert.IsTrue(comparer.Compare(default, default) == 0);
+        }
+
+        [TestMethod]
+        public void CanCompareNulls()
+        {
+            var comparer = ComparerFactory<User>.Create(x => x.Age);
+            
+            Assert.IsTrue(comparer.Compare(default, new User(0)) < 0);
+            Assert.IsTrue(comparer.Compare(new User(0), default) > 0);
+            Assert.IsTrue(comparer.Compare(default, default) == 0);
+        }
+
+        private class User
+        {
+            public User(int age) => Age = age;
+
+            public int Age { get; }
+        }
+    }
+}
