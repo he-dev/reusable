30b9bd6e8a24c5c8b2eef6f68bbecae068633790
ᎬᏁᎶᎥᏁᎬᎬᏒ <bennyboss+github@gmail.com>
2018-11-25 21:30:35 +0100

    Improve html-table

---
+++ Reusable.sdk.Mailr/src/Models/HtmlTable.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+
+namespace Reusable.sdk.Mailr.Models
+{
+    public class HtmlTable
+    {
+        public HtmlTable(IList<HtmlTableColumn> columns)
+        {
+            Head = new HtmlTableSection(columns);
+            var row = Head.NewRow();
+            foreach (var column in columns)
+            {
+                row[column.Name].Data = column.Name.ToString();
+            }
+
+            Body = new HtmlTableSection(columns);
+            Foot = new HtmlTableSection(columns);
+        }
+
+        [NotNull]
+        public HtmlTableSection Head { get; }
+
+        [NotNull]
+        public HtmlTableSection Body { get; }
+
+        [NotNull]
+        public HtmlTableSection Foot { get; }
+
+        #region JsonNet extensions
+
+        public bool ShouldSerializeHead() => Head.Any();
+        public bool ShouldSerializeBody() => Body.Any();
+        public bool ShouldSerializeFoot() => Foot.Any();
+
+        #endregion
+    }
+
+    public class HtmlTableSection : List<HtmlTableRow>
+    {
+        public HtmlTableSection(IEnumerable<HtmlTableColumn> columns)
+        {
+            Columns = columns.Select((column, ordinal) => new HtmlTableColumn
+            {
+                Name = column.Name,
+                Ordinal = ordinal,
+                Type = column.Type
+            }).ToList();
+        }
+
+        [JsonIgnore]
+        public IEnumerable<HtmlTableColumn> Columns { get; }
+    }
+
+    public class HtmlTableColumn
+    {
+        public SoftString Name { get; internal set; }
+
+        public int Ordinal { get; internal set; }
+
+        public Type Type { get; internal set; }
+
+        internal static HtmlTableColumn Create<T>(SoftString name) => new HtmlTableColumn
+        {
+            Name = name,
+            Type = typeof(T)
+        };
+
+        internal static IList<HtmlTableColumn> Create(params (string Name, Type Type)[] columns)
+        {
+            return columns.Select(x => new HtmlTableColumn
+            {
+                Name = x.Name,
+                Type = x.Type
+            })
+            .ToList();
+        }
+
+        public override string ToString() => $"{Name}[{Ordinal}]";
+    }
+
+    public class HtmlTableRow : IEnumerable<HtmlTableCell>
+    {
+        private readonly IList<HtmlTableCell> _cells;
+
+        internal HtmlTableRow(IEnumerable<HtmlTableColumn> columns)
+        {
+            // All rows need to have the same length so initialize them with 'default' values.
+            _cells = columns.Select(column => new HtmlTableCell(column)).ToList();
+        }
+
+        [JsonIgnore]
+        [NotNull]
+        public HtmlTableCell this[SoftString name] => this.Single(cell => cell.Column.Name.Equals(name));
+
+        [JsonIgnore]
+        [NotNull]
+        public HtmlTableCell this[int ordinal] => this.Single(cell => cell.Column.Ordinal.Equals(ordinal));
+
+        public IEnumerator<HtmlTableCell> GetEnumerator() => _cells.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_cells).GetEnumerator();
+    }
+
+    public class HtmlTableCell
+    {
+        public HtmlTableCell(HtmlTableColumn column)
+        {
+            Column = column;
+        }
+
+        [JsonIgnore]
+        public HtmlTableColumn Column { get; }
+
+        public IList<string> Styles { get; } = new List<string>();
+
+        public object Data { get; set; }
+
+        #region JsonNet extensions
+
+        public bool ShouldSerializeStyles() => Styles.Any();
+
+        #endregion
+    }
+
+    public static class HtmlTableSectionExtensions
+    {
+        [NotNull]
+        public static HtmlTableRow NewRow(this HtmlTableSection section)
+        {
+            var newRow = new HtmlTableRow(section.Columns);
+            section.Add(newRow);
+            return newRow;
+        }
+
+        public static void Add(this HtmlTableSection table, IEnumerable<object> values)
+        {
+            var newRow = table.NewRow();
+            foreach (var (column, value) in table.Columns.Zip(values, (column, value) => (column, value)))
+            {
+                newRow[column.Name].Data = value;
+            }
+        }
+
+        public static void Add(this HtmlTableSection table, params object[] values) => table.Add((IEnumerable<object>)values);
+    }
+
+    public static class HtmlTableRowExtensions
+    {
+        public static T Value<T>(this HtmlTableRow row, SoftString name) => row[name] is T value ? value : default;
+
+        public static T Value<T>(this HtmlTableRow row, int ordinal) => row[ordinal] is T value ? value : default;
+    }
+}
