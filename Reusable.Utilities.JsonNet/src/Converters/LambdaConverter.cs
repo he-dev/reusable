9b683cbc79fa9e3dbe7db3edfe0ffd383668fba0
ᎬᏁᎶᎥᏁᎬᎬᏒ <bennyboss+github@gmail.com>
2019-04-03 17:20:51 +0200

    Update LambdaConverter.cs

--- Reusable.Utilities.JsonNet/src/Converters/LambdaConverter.cs
+++ Reusable.Utilities.JsonNet/src/Converters/LambdaConverter.cs
@@ -4,20 +4,20 @@ using Newtonsoft.Json;
 namespace Reusable.Utilities.JsonNet.Converters
 {
     public delegate object WriteJsonCallback<in T>(T value);
-    
+
     public class LambdaConverter<T> : JsonConverter<T>
     {
         private readonly WriteJsonCallback<T> _writeJsonCallback;
-        
+
         public LambdaConverter(WriteJsonCallback<T> writeJsonCallback)
         {
             _writeJsonCallback = writeJsonCallback;
         }
-        
+
         public static LambdaConverter<T> Create(WriteJsonCallback<T> convert) => new LambdaConverter<T>(convert);
 
         public override void WriteJson(JsonWriter writer, T value, JsonSerializer serializer)
-        {            
+        {
             serializer.Serialize(writer, _writeJsonCallback(value));
         }
 
