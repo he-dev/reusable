89cb33b4ab2b1751bde41a944722ffca270819c7
ᎬᏁᎶᎥᏁᎬᎬᏒ <bennyboss+github@gmail.com>
2019-05-01 15:35:54 +0200

    Clean-up tests

---
+++ Reusable.Tests.XUnit/src/Flexo/ExpressionFixture.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Autofac;
+using JetBrains.Annotations;
+using Reusable.Flexo;
+using Reusable.IOnymous;
+using Reusable.OmniLog;
+using Reusable.Utilities.JsonNet;
+using Reusable.Utilities.JsonNet.DependencyInjection;
+
+namespace Reusable.Tests.Flexo
+{
+    [UsedImplicitly]
+    public class ExpressionFixture : IDisposable
+    {
+        private static readonly IResourceProvider Flexo =
+            EmbeddedFileProvider<ExpressionSerializerTest>
+                .Default
+                .DecorateWith(RelativeProvider.Factory(@"res\Flexo"));
+
+        private readonly ILifetimeScope _scope;
+        private readonly IDisposable _disposer;
+        private readonly ConcurrentDictionary<string, IList<IExpression>> _expressions = new ConcurrentDictionary<string, IList<IExpression>>();
+
+        public ExpressionFixture()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule<JsonContractResolverModule>();
+            builder.RegisterModule(new LoggerModule(new LoggerFactory()));
+            builder.RegisterModule(new ExpressionSerializerModule(Expression.Types));
+
+            var container = builder.Build();
+            _scope = container.BeginLifetimeScope();
+            _disposer = Disposable.Create(() =>
+            {
+                _scope.Dispose();
+                container.Dispose();
+            });
+
+            Serializer = _scope.Resolve<ExpressionSerializer.Factory>()(TypeDictionary.From(Expression.Types));
+        }
+
+        public IExpressionSerializer Serializer { get; }
+
+        public T Resolve<T>(Action<T> configure = default)
+        {
+            var t = _scope.Resolve<T>();
+            configure?.Invoke(t);
+            return t;
+        }
+
+        public IList<IExpression> GetReferences()
+        {
+            return _expressions.GetOrAdd("ExpressionReferences.json", ReadExpressionFile());
+        }
+
+
+        public IList<IExpression> GetExpressions()
+        {
+            return _expressions.GetOrAdd("ExpressionCollection.json", ReadExpressionFile());
+        }
+
+        private Func<string, IList<IExpression>> ReadExpressionFile()
+        {
+            return n =>
+            {
+                var json = Flexo.ReadTextFile(n);
+                return Serializer.Deserialize<IList<IExpression>>(json);
+            };
+        }
+
+        public void Dispose()
+        {
+            _disposer.Dispose();
+        }
+    }
+}
--- Reusable.Tests.XUnit/src/Flexo/ExpressionSerializerTest.cs
+++ Reusable.Tests.XUnit/src/Flexo/ExpressionSerializerTest.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.PeerToPeer;
 using System.Reflection;
 using System.Threading.Tasks;
 using Autofac;
-using JetBrains.Annotations;
 using Reusable.Data;
 using Reusable.Exceptionize;
 using Reusable.Extensions;
@@ -24,73 +22,7 @@ using ExpressionSerializer = Reusable.Flexo.ExpressionSerializer;
 // ReSharper disable once CheckNamespace
 namespace Reusable.Tests.Flexo
 {
-    [UsedImplicitly]
-    public class ExpressionFixture : IDisposable
-    {
-        private static readonly IResourceProvider Flexo =
-            EmbeddedFileProvider<ExpressionSerializerTest>
-                .Default
-                .DecorateWith(RelativeProvider.Factory(@"res\Flexo"));
-
-        private readonly ILifetimeScope _scope;
-        private readonly IDisposable _disposer;
-        private readonly IExpressionSerializer _serializer;
-        private readonly ConcurrentDictionary<string, IList<IExpression>> _expressions = new ConcurrentDictionary<string, IList<IExpression>>();
-
-        public ExpressionFixture()
-        {
-            var builder = new ContainerBuilder();
-
-            builder.RegisterModule<JsonContractResolverModule>();
-            builder.RegisterModule(new LoggerModule(new LoggerFactory()));
-            builder.RegisterModule(new ExpressionSerializerModule(Expression.Types));
-
-            var container = builder.Build();
-            _scope = container.BeginLifetimeScope();
-            _disposer = Disposable.Create(() =>
-            {
-                _scope.Dispose();
-                container.Dispose();
-            });
-
-            _serializer = _scope.Resolve<ExpressionSerializer.Factory>()(TypeDictionary.From(Expression.Types));
-        }
-
-
-        public T Resolve<T>(Action<T> configure = default)
-        {
-            var t = _scope.Resolve<T>();
-            configure?.Invoke(t);
-            return t;
-        }
-
-        public IList<IExpression> GetReferences()
-        {
-            return _expressions.GetOrAdd("ExpressionReferences.json", ReadExpressionFile());
-        }
-
-
-        public IList<IExpression> GetExpressions()
-        {
-            return _expressions.GetOrAdd("ExpressionCollection.json", ReadExpressionFile());
-        }
-
-        private Func<string, IList<IExpression>> ReadExpressionFile()
-        {
-            return n =>
-            {
-                var json = Flexo.ReadTextFile(n);
-                return _serializer.Deserialize<IList<IExpression>>(json);
-            };
-        }
-
-        public void Dispose()
-        {
-            _disposer.Dispose();
-        }
-    }
-
-    public class ExpressionSerializerTest : IDisposable, IClassFixture<ExpressionFixture>
+    public class ExpressionSerializerTest : IClassFixture<ExpressionFixture>
     {
         private readonly ExpressionFixture _helper;
         private readonly ITestOutputHelper _output;
@@ -100,36 +32,18 @@ namespace Reusable.Tests.Flexo
                 .Default
                 .DecorateWith(RelativeProvider.Factory(@"res\Flexo"));
 
-        private readonly IExpressionSerializer _serializer;
-
-        private readonly IDisposable _container;
 
         public ExpressionSerializerTest(ExpressionFixture helper, ITestOutputHelper output)
         {
             _helper = helper;
             _output = output;
-            var builder = new ContainerBuilder();
-
-            builder.RegisterModule<JsonContractResolverModule>();
-            builder.RegisterModule(new LoggerModule(new LoggerFactory()));
-            builder.RegisterModule(new ExpressionSerializerModule(Expression.Types));
-
-            var container = builder.Build();
-            var scope = container.BeginLifetimeScope();
-
-            _serializer = scope.Resolve<ExpressionSerializer.Factory>()(TypeDictionary.From(Expression.Types));
-            _container = Disposable.Create(() =>
-            {
-                scope.Dispose();
-                container.Dispose();
-            });
         }
 
         [Theory]
         [SmartMemberData(nameof(GetData))]
         public void Can_evaluate_supported_expressions(string useCaseName, object expected, bool throws)
         {
-            var useCase = _helper.GetExpressions().Get(useCaseName);
+            var useCase = _helper.GetExpressions().Single(e => e.Name == useCaseName);
             ExpressionAssert.Equal(expected, useCase, ctx => ctx.WithReferences(_helper.GetReferences()), _output, throws);
         }
 
@@ -139,7 +53,7 @@ namespace Reusable.Tests.Flexo
             var jsonFile = await Flexo.GetFileAsync(@"ExpressionObject.json", MimeType.Json);
             using (var jsonStream = await jsonFile.CopyToMemoryStreamAsync())
             {
-                var expression = await _serializer.DeserializeExpressionAsync(jsonStream.Rewind());
+                var expression = await _helper.Serializer.DeserializeExpressionAsync(jsonStream.Rewind());
                 Assert.NotNull(expression);
             }
         }
@@ -168,19 +82,6 @@ namespace Reusable.Tests.Flexo
             ("Collection.Overlaps{Comparer=SoftString}", true, false),
             ("Collection.Select.ToString", new[] { "1", "True" }, false),
         }.Select(uc => new { uc.UseCaseName, uc.Expected, uc.Throws });
-
-        public void Dispose()
-        {
-            _container.Dispose();
-        }
-    }
-
-    internal static class EnumerableExtensions
-    {
-        public static IExpression Get(this IEnumerable<IExpression> expressions, SoftString name)
-        {
-            return expressions.Single(e => e.Name == name);
-        }
     }
 
     [DataDiscoverer("Xunit.Sdk.MemberDataDiscoverer", "xunit.core")]
@@ -199,7 +100,11 @@ namespace Reusable.Tests.Flexo
                 var tmp = testMethodParameters[i];
                 if (itemProperties.TryGetValue(tmp.Name, out var ip))
                 {
-                    if (!tmp.ParameterType.IsAssignableFrom(ip.PropertyType))
+                    if (tmp.ParameterType.IsAssignableFrom(ip.PropertyType))
+                    {
+                        dataItem[i] = ip.GetValue(item);
+                    }
+                    else
                     {
                         throw DynamicException.Create
                         (
@@ -209,21 +114,20 @@ namespace Reusable.Tests.Flexo
                             $"to the parameter '{tmp.Name}' of type '{tmp.ParameterType.ToPrettyString()}'."
                         );
                     }
-                    dataItem[i] = ip.GetValue(item);
                 }
                 else
-                {                    
-                    if (!tmp.IsOptional)
+                {
+                    if (tmp.IsOptional)
+                    {
+                        dataItem[i] = tmp.DefaultValue;
+                    }
+                    else
                     {
                         throw DynamicException.Create
                         (
                             $"DataItemParameterNotOptional",
                             $"Data item for '{GetTestMethodInfo()}' does not specify the required parameter '{tmp.Name}'"
                         );
-                    }
-                    else
-                    {
-                        dataItem[i] = tmp.DefaultValue;
                     }
                 }
             }
