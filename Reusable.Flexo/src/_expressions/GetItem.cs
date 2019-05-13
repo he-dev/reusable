a82e683b89a4249f0f40975b1cc7cf2054fdb627
he-dev <bennyboss+github@gmail.com>
2019-05-13 12:26:50 +0200

    Fix cannot access field that acts as a property

--- Reusable.Flexo/src/_expressions/GetItem.cs
+++ Reusable.Flexo/src/_expressions/GetItem.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Linq.Custom;
+using System.Reflection;
 using System.Xml;
 using JetBrains.Annotations;
 using Newtonsoft.Json;
@@ -37,7 +38,11 @@ namespace Reusable.Flexo
                         ? item
                         : throw DynamicException.Create("ItemNotFound", $"Could not find an item with the key '{Path}'.");
 
-            obj = names.Skip(1).Aggregate(obj, (current, name) => current.GetType().GetProperty(name).GetValue(current));
+            obj = names.Skip(1).Aggregate(obj, (current, name) =>
+                current is IConstant constant
+                    ? constant.Value.GetType().GetProperty(name)?.GetValue(constant.Value) ?? constant.Value.GetType().GetField(name)?.GetValue(constant.Value)
+                    : current.GetType().GetProperty(name)?.GetValue(current) ?? current.GetType().GetField(name)?.GetValue(current)
+            );
 
             return obj;
         }
