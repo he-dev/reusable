d1cec67c0c026d7488ceef5d4e0360cdf430766d
he-dev <bennyboss+github@gmail.com>
2019-08-09 08:37:25 +0200

    Improve log-entry-key handling

--- Reusable.Tests/src/OmniLog/LoggerNodeTest.cs
+++ Reusable.Tests/src/OmniLog/LoggerNodeTest.cs
@@ -21,14 +21,14 @@ namespace Reusable.OmniLog
                 {
                     node.Invoke(logEntry);
                     
-                    Assert.True(logEntry.TryGetItem<List<CorrelationNode.Scope>>(CorrelationNode.DefaultLogEntryItemNames.Scope, SerializationNode.LogEntryItemTags.Serializable, out var scope));
+                    Assert.True(logEntry.TryGetItem<List<CorrelationNode.Scope>>(node.Key, out var scope));
                     Assert.Equal(1, scope.Count);
                     
                     using (node.Push((CorrelationId: "scope-1", CorrelationHandle: "handle-1")))
                     {
                         node.Invoke(logEntry);
                     
-                        Assert.True(logEntry.TryGetItem<List<CorrelationNode.Scope>>(CorrelationNode.DefaultLogEntryItemNames.Scope, SerializationNode.LogEntryItemTags.Serializable, out scope));
+                        Assert.True(logEntry.TryGetItem<List<CorrelationNode.Scope>>(node.Key, out scope));
                         Assert.Equal(2, scope.Count);
                     }
                 }
